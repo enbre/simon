ac64a9cb74d2bf5931ed689262714d05bc3a5eb4
enbre <noahbrezel@gmail.com>
2021-11-18 17:05:55 -0800

    trying to get sections to light up and turn off in sequence. Not yet successful

--- main.js
+++ main.js
@@ -2,48 +2,47 @@ console.log("insanity check");
 // let body = window.document.getElementsByTagName("body")[0];
 // var document;
 let userHighScore = 0;
+let compPattern = []
 
 const game = () => {
    // game variables:
-   const compPattern = [],
+   let compPatternDummy = [2,1,0,0,3,0,1], // yellow, red, green, green, blue, green, red
       // userPattern = [],
       userCurrentScore = 0;
-
    // round: 
    // - computer's turn
    // - user's turn
-   // 
-
 
    // each round, the computer pattern gets one digit longer using the digits 0-3 
    const compPatternPicker = () => {
       let newSelectIDX = Math.floor(Math.random() * 4)
       compPattern.push(newSelectIDX)
-      console.log('test', newSelectIDX, compPattern)
+      // console.log('after', newSelectIDX, compPatternDummy)
    }
    // -light up and turn off one game section
    const lightUpSection = (idx) => {
       let selection = document.getElementById(`section-${idx}`)
-      selection.classList.add("lit")
+      setTimeout(() =>{
+         selection.classList.add("lit");
+      },600);
       setTimeout(() => {
          selection.classList.remove("lit");
-      }, 300);
+      }, 600);
    }
    
    // computer turn:
    const compTurn = () => {
       // -trigger the "computer's turn" modal
-      // -add a new selection to compPattern
       compPatternPicker()
+      // compPatternPicker()
       // -light up and turn off each game section in order of compPattern
-
-      
-      for (let i in compPattern) {
-         // console.log(i, compPattern[i], compPattern)
-         lightUpSection(compPattern[i])
+      console.log('pattern', compPatternDummy)
+      for (let i in compPatternDummy) {
+         lightUpSection(compPatternDummy[i])
       }
       // -remove the "computer's turn" modal
    }
+   compTurn()
    
    // user's turn:
    // -as user selects each section, check it against the corrosponding index in compPattern
@@ -55,4 +54,42 @@ const game = () => {
    // -keep repeating rounds until the user makes a mistake
    // -userHighScore = Math.max(userCurrentScore, userHighScore)
 }
-game();
+
+let compPatternDummy = [2,1]
+// let compPatternDummy = [2,1,0,0,3,0,1]
+
+const lightUpSection = (idx) => {
+   let selection = document.getElementById(`section-${idx}`)
+   setTimeout(() =>{
+      selection.classList.add("lit");
+      console.log('add',selection)
+   },300);
+   setTimeout(() => {
+   console.log('remove',selection)
+   selection.classList.remove("lit");
+   }, 2000);
+   
+}   
+const lightUpSectionRemove = (idx) => {
+   let selection = document.getElementById(`section-${idx}`)
+      setTimeout(() => {
+      console.log('remove',selection)
+      selection.classList.remove("lit");
+   }, 2000);
+}   
+
+
+
+const compTurn = () => {
+   for (let i in compPatternDummy) {
+      console.log('test', i, compPatternDummy[i])
+      // setTimeout(()=>{
+
+         lightUpSection(compPatternDummy[i])
+      // },1000)
+      // setTimeout(() => {
+         // lightUpSectionRemove(compPatternDummy[i])
+
+      // },2000)
+   }
+}
