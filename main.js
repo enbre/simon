d9c19bbbcef9cb6a71308d3b82ecba7b36ab5b91
enbre <noahbrezel@gmail.com>
2021-12-17 16:57:53 -0800

    cleans up styling for gameover modal

--- main.js
+++ main.js
@@ -3,137 +3,133 @@ console.log("insanity check");
 let highScore = 0;
 
 // const game = () => {
-   // game variables:
-   let compPattern = [];
-   let userScore = 0;
-   let compPatternDummy = [2, 1, 0, 0, 3, 0, 1] // yellow, red, green, green, blue, green, red
+// game variables:
+let compPattern = [];
+let userScore = 0;
+let compPatternDummy = [2, 1, 0, 0, 3, 0, 1] // yellow, red, green, green, blue, green, red
 
-   // each round, the computer pattern gets one digit longer using the digits 0-3 
-   const compPatternPicker = () => {
-      let newSelectIDX = Math.floor(Math.random() * 4)
-      compPattern.push(newSelectIDX)
-   }
+// each round, the computer pattern gets one digit longer using the digits 0-3 
+const compPatternPicker = () => {
+   let newSelectIDX = Math.floor(Math.random() * 4)
+   compPattern.push(newSelectIDX)
+}
 
-   const toggleLight = (idx) => {
-      let selection = document.getElementById(`${idx}`)
-      selection.classList.toggle('lit')
-   }
-   const toggleModal = () => {
-      let selection = document.getElementById('myModal')
-      selection.classList.toggle('modal')
-   }
+const toggleLight = (idx) => {
+   let selection = document.getElementById(`${idx}`)
+   selection.classList.toggle('lit')
+}
+const toggleModal = () => {
+   let selection = document.getElementById('myModal')
+   selection.classList.toggle('modal')
+}
 
-   // -light up and turn off one game section
-   const lightUpSection = (idx, interval) => {
-      setTimeout(() => {
-         toggleLight(idx)
-      }, interval)
-      setTimeout(() => {
-         toggleLight(idx)
-      }, interval + 500)
+// -light up and turn off one game section
+const lightUpSection = (idx, interval) => {
+   setTimeout(() => {
+      toggleLight(idx)
+   }, interval)
+   setTimeout(() => {
+      toggleLight(idx)
+   }, interval + 500)
+}
+// light up each section in compPattern- might need to rename:
+const lightUpCompPattern = (interval) => {
+   for (let i in compPattern) {
+      lightUpSection(compPattern[i], interval)
+      interval += 1000;
    }
-   // light up each section in compPattern- might need to rename:
-   const lightUpCompPattern = (interval) => {
-      for (let i in compPattern) {
-         lightUpSection(compPattern[i], interval)
-         interval += 1000;
-      }
-      console.log('compPattern',compPattern)
-   }
+   console.log('compPattern', compPattern)
+}
 
-   // computer turn:
-   const compTurn = () => {
-      // disable user from clicking
-      window.removeEventListener('click', userTurn)
-      // reset timing variable
-      let interval = 500;
-      // dim background
+// computer turn:
+const compTurn = () => {
+   // disable user from clicking
+   window.removeEventListener('click', userTurn)
+   // reset timing variable
+   let interval = 500;
+   // dim background
+   toggleModal()
+   // add new index to compPattern
+   compPatternPicker();
+   // light up all sections in compPattern
+   lightUpCompPattern(interval);
+   // remove dim background after whole pattern has run
+   setTimeout(() => {
       toggleModal()
-      // add new index to compPattern
-      compPatternPicker();
-      // light up all sections in compPattern
-      lightUpCompPattern(interval);
-      // remove dim background after whole pattern has run
-      setTimeout(()=>{
-         toggleModal()
-      },(interval + 1000 * compPattern.length))
-      // enable user to click
-      window.addEventListener('click', userTurn)
-      console.log('end of comp turn')
-   }
+   }, (interval + 1000 * compPattern.length))
+   // enable user to click
+   window.addEventListener('click', userTurn)
+   console.log('end of comp turn')
+}
 
-   // user's turn:
-   // -as user selects each section, check it against the corrosponding index in compPattern
+// user's turn:
+// -as user selects each section, check it against the corrosponding index in compPattern
+// -if they don't match, trigger a "game over" modal with a "play again" button
+// -increment user score
 
+// Figures out which section was clicked and compares it to the index in the compPattern. If it doesn't match, the game is over.
+const userTurn = (event) => {
+   let count = 0;
+   
+   if (event.target.nodeName === 'SECTION') {
+      // figure out which section the user has selected.
+      let clicked = parseInt(event.target.id)
+      console.log('clicked section:', clicked)
+      if (clicked !== compPattern[count]) {
+         console.log("count:", count, 'compPattern:', compPattern)
+         gameOver()
+         return
+      }
+      lightUpSection(clicked, 200)
 
-   // -if they don't match, trigger a "game over" modal with a "play again" button
-   // -increment user score
-
-
-
-   // Figures out which section was clicked and compares it to the index in the compPattern. If it doesn't match, the game is over.
-   const userTurn = (event) => {
-      let count = 0;
-
-      if (event.target.nodeName === 'SECTION') {
-            // figure out which section the user has selected.
-         let clicked = parseInt(event.target.id)
-         console.log('clicked section:', clicked)
-         lightUpSection(clicked, 200)
-         for (let i in compPattern){
-         // }
-         console.log('the count is:' ,count)
-         if (clicked !== compPattern[count]) {
-            console.log("count:", count, 'compPattern:', compPattern)
-            gameOver()
-            return
-         }
-         count++;
+      for (let i = 0;i<compPattern.length;i++) {
+      // while (count < compPattern.length) {
+         console.log('status:',  count, compPattern.length)
+         // count++;
          // increment userScore and update current and high scores on screen
          userScore++;
          updateScores()
-         // console.log('length',compPattern.length)
          // once user has matched the entire computer pattern, their turn is over
-      }
-      }
-         if(compPattern.length === count) {
+         if (compPattern.length === count+1) {
             console.log('next round')
-            setTimeout(()=>{
+            setTimeout(() => {
                compTurn()
-            },1500)
-         // }
-         console.log('end of user turn')
+            }, 1500)
+            // }
+            console.log('end of user turn')
+         }
+         // i++
       }
-      // count++;
-   };
-   // const userClick = (event) => {
-
-   // }
-
-
-
-
-   const updateScores = ()=>{
-      let showScore = document.getElementById('userScore');
-      showScore.innerText=`Score: ${userScore}`
-      highScore = Math.max(userScore, highScore)
-      let showHighScore = document.getElementById('highScore');
-      showHighScore.innerText=`High Score: ${highScore}`
+      count++;
    }
-   const gameOver = () =>{
-      userScore = 0;
-      console.log("Game Over!! Poop!!")
-      let selection = document.getElementById('myModal')
-      selection.classList.add('modal-game-over')
-      let gameStatus = document.getElementById('gameStatus');
-      gameStatus.innerText='Game Over!!!'
+};
+// const userClick = (event) => {
 
-   }
-
-   // game:
-   // -reset compPattern to empty array, userCurrentScore to 0;
-   // -keep repeating rounds until the user makes a mistake
-   //
-// end of game()
 // }
 
+
+
+
+const updateScores = () => {
+   let showScore = document.getElementById('userScore');
+   showScore.innerText = `Score: ${userScore}`
+   highScore = Math.max(userScore, highScore)
+   let showHighScore = document.getElementById('highScore');
+   showHighScore.innerText = `High Score: ${highScore}`
+}
+const gameOver = () => {
+   userScore = 0;
+   console.log("Game Over!! Poop!!")
+   let selection = document.getElementById('myModal')
+   selection.classList.add('modal-game-over')
+   let gameStatus = document.getElementById('gameStatus');
+   gameStatus.innerText = 'Game Over!!!'
+
+}
+
+// game:
+// -reset compPattern to empty array, userCurrentScore to 0;
+// -keep repeating rounds until the user makes a mistake
+//
+// end of game()
+// }
