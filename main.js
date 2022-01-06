bad2f718f03ce99cdd73c5c272c8f3be08f51f03
enbre <noahbrezel@gmail.com>
2022-01-06 15:17:38 -0800

    game is fully functional

--- main.js
+++ main.js
@@ -9,7 +9,6 @@ let userScore = 0;
 let count = 0;
 let base = 1000;
 
-let compPatternDummy = [2, 1, 0, 0, 3, 0, 1] // yellow, red, green, green, blue, green, red
 
 // each round, the computer pattern gets one digit longer using the digits 0-3 
 const compPatternPicker = () => {
@@ -21,7 +20,7 @@ const toggleLight = (idx) => {
    let selection = document.getElementById(`${idx}`)
    selection.classList.toggle('lit')
 }
-// not currently using this:
+// not currently using this, but would like to figure out why ternary doesn't work:
 // const toggleModal = () => {
 //    let selection = document.getElementById('compTurnModal')
 //    console.log('display:',selection.style.display)
@@ -32,12 +31,14 @@ const toggleLight = (idx) => {
 const lightUpSection = (idx, interval) => {
    setTimeout(() => {
       toggleLight(idx)
+      // console.log('on',idx)
    }, interval)
    setTimeout(() => {
       toggleLight(idx)
+      // console.log('off', idx)
    }, interval + base/2)
 }
-// light up each section in compPattern- might need to rename:
+// light up each section in compPattern
 const lightUpCompPattern = (interval) => {
    for (let i in compPattern) {
       lightUpSection(compPattern[i], interval)
@@ -51,7 +52,7 @@ const compTurn = () => {
    // disable user from clicking
    window.removeEventListener('click', userClick)
    // reset timing variable
-   let interval = 500;
+   interval = 500;
    // remove start button
    let start = document.getElementById('start')
    start.style.display='none'
@@ -81,17 +82,14 @@ const changeGameStatus = (input) => {
 
 const userClick = (event) => {
    if (event.target.nodeName === 'SECTION') {
-      // figure out which section the user has selected.
-      console.log(compPattern, "compPattern[count]:", compPattern[count], 'count:', count)
       let clicked = parseInt(event.target.id)
-      console.log('clicked section:',clicked)
       if (clicked !== compPattern[count]) {
          console.log("count:", count, 'compPattern:', compPattern)
          gameOver()
          return
       }
       count++;
-      console.log('compare:', compPattern.length, count)
+      lightUpSection(clicked, base/4)
       if (compPattern.length === count) {
          console.log('next round')
          userScore++;
@@ -102,7 +100,6 @@ const userClick = (event) => {
          console.log('end of user turn')
          return;
       }
-      lightUpSection(clicked, base)
    }
 }
 
@@ -111,8 +108,6 @@ const userTurn = () => {
    count = 0;
    // allows user to click
    window.addEventListener('click', userClick)
-   // console.log('count:', count, "compPattern Length:", compPattern.length)
-   
 };
 
 const updateScores = () => {
@@ -127,8 +122,6 @@ const gameOver = () => {
    console.log("Game Over!! Poop!!")
    let selection = document.getElementById('gameOverModal')
    selection.style.display = 'flex'
-   // let gameStatus = document.getElementById('gameStatus');
-   // gameStatus.innerText = 'Game Over!!!'
 }
 
 const newGame = () => {
